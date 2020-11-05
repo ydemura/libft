0ff9e28527adbc27f792624b2926de243c9c3a06
Yuliia Demura <ydemura@student.codam.nl>
2020-11-05 08:49:01 +0100

    norm groove f_split

--- lib_ft/functions/ft_split.c
+++ lib_ft/functions/ft_split.c
@@ -14,89 +14,59 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int		ft_delimiter_counter(char const *s, char c)
+int		ft_char_count(char const *s, char c)
 {
-	int		len_s;
-	int		delimeter_n;
+	int		result;
 
-	len_s = 0;
-	delimeter_n = 0;
-	while (s[len_s] != 0)
-	{
-		if (s[len_s] == c)
-		{
-			delimeter_n++;
-		}
-		len_s++;
-	}
-	return (delimeter_n);
-}
-
-int		ft_malloc_check(char *substring, int next_result_index, char **results)
-{
-	if (substring == NULL)
-	{
-		while (next_result_index > 0)
-		{
-			free(results[next_result_index]);
-			next_result_index--;
-		}
-		free(results);
-		return (0);
-	}
-	return (1);
-}
-
-char	**ft_do_split(char const *s, char c, char **res, const char *curr_ptr)
-{
-	int			next_result_index;
-	char		*substring;
-	size_t		len_substr;
-
-	next_result_index = 0;
+	result = 0;
 	while (*s != 0)
 	{
-		if (*curr_ptr == c || *curr_ptr == 0)
+		if (*s == c)
 		{
-			len_substr = curr_ptr - s;
-			substring = (char *)malloc((len_substr + 1) * sizeof(char));
-			if (ft_malloc_check(substring, next_result_index, res) == 0)
-			{
-				return (NULL);
-			}
-			else
-			{
-				substring[len_substr] = '\0';
-				res[next_result_index] = ft_memcpy(substring, s, len_substr);
-				next_result_index++;
-				if (*curr_ptr == 0)
-				{
-					res[next_result_index] = 0;
-					s = curr_ptr;
-				}
-				else
-				{
-					s = curr_ptr + 1;
-				}
-			}
+			result++;
 		}
-		curr_ptr++;
+		s++;
 	}
-	return (res);
+	return (result);
+}
+
+int	ft_subsstr_until_char(char **substring, const char **start, char c)
+{
+	const char *end;
+
+	end = *start;
+	while (*end != 0 && *end != c)
+		end++;
+	*substring = malloc((end - *start + 1) * sizeof(char));
+	if (*substring == NULL)
+		return (0);
+	ft_memcpy(*substring, *start, end - *start);
+	*start = end + 1;
+	return (1);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int			delimeter_n;
+	int			n_substrings;
+	int			i_substrings;
 	char		**results;
 
-	delimeter_n = ft_delimiter_counter(s, c);
-	results = (char **)malloc((delimeter_n + 2) * sizeof(char *));
+	i_substrings = 0;
+	n_substrings = ft_char_count(s, c) + 1;
+	results = (char **)malloc((n_substrings + 1) * sizeof(char *));
 	if (results == NULL)
-	{
 		return (NULL);
-	}
-	results = ft_do_split(s, c, results, s);
+	while (i_substrings < n_substrings)
+		if (ft_subsstr_until_char(&results[i_substrings], &s, c) == 1)
+			i_substrings++;
+		else
+		{
+			while (i_substrings >= 0)
+				free(results[i_substrings--]);
+			free(results);
+			return (NULL);
+		}
+	results[n_substrings] = NULL;
 	return (results);
 }
 
